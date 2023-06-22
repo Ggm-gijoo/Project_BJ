7a4a35bf0422efa2ab28d4717f0c08f9c2c82dfe
moonkanghyuck <moonkanghyuck@gmail.com>
2023-06-22 17:35:05 +0900

    0622_마커 버그 수정중

--- Assets/04.Scripts/Marker/BaseMarker.cs
+++ Assets/04.Scripts/Marker/BaseMarker.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Marker
 {
     public class BaseMarker : MonoBehaviour
     {
+        [SerializeField] private LineRenderer lineRenderer;
+        [SerializeField] private EdgeCollider2D edgeCollider2D;
+        private bool isDrawComplete;
+        
         public virtual void OnBeginDraw()
         {
             
@@ -17,6 +22,41 @@ namespace Marker
         
         public virtual void OnEndDraw()
         {
+            isDrawComplete = true;
+        }
+
+        public void Update()
+        {
+            if (!isDrawComplete)
+            {
+                return;
+            }
+
+            //UpdatePositionFromTransform();
+        }
+
+        private void UpdatePositionFromTransform()
+        {
+            int _index = 0;
+            Vector3[] _positions = new Vector3[lineRenderer.positionCount];
+            lineRenderer.GetPositions(_positions);
+            foreach (var pos in _positions)
+            {
+                Vector3 _newPos = transform.localToWorldMatrix * new Vector4(pos.x, pos.y, pos.z, 1);
+                _positions[_index] = _newPos;
+                lineRenderer.SetPosition(_index++, _newPos);
+            }
+            edgeCollider2D.points = toVector2Array(_positions);
+        }
+        
+        public static Vector2[] toVector2Array (Vector3[] v3)
+        {
+            return System.Array.ConvertAll<Vector3, Vector2> (v3, getV3fromV2);
+        }
+        
+        public static Vector2 getV3fromV2 (Vector3 v3)
+        {
+            return new Vector2 (v3.x, v3.y);
         }
         
     }
--- Assets/04.Scripts/Marker/SpecificMarker/GravityMarker.cs
+++ Assets/04.Scripts/Marker/SpecificMarker/GravityMarker.cs
@@ -10,12 +10,23 @@ namespace Marker
         private Rigidbody2D rigid;
         [SerializeField]
         private EdgeCollider2D edgeCollider2D;
+        [SerializeField]
+        private PolygonCollider2D polygonCollider2D;
+        
         
         public override void OnEndDraw()
         {
+            base.OnEndDraw();
             edgeCollider2D.enabled = true;
+
+            //polygonCollider2D.points = edgeCollider2D.points;
             rigid.isKinematic = false;
         }
+        
+        //public void Update()
+        //{
+        //    
+        //}
     }
 
 }
